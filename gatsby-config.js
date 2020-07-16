8a3c64d89f237cc5e4a6a96783a434891f6945b0
Jason Beh <behjieshen@gmail.com>
2020-07-16 12:35:01 +0800

    changed details in gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Casper',
-    description: 'A port of the casper blog built for gatsby',
-    siteUrl: 'https://gatsby-casper.netlify.com', // full path to blog - no ending slash
+    title: 'Jason Beh',
+    description: 'A software engineer blog and portfolio',
+    siteUrl: 'https://jasonbeh.com', // full path to blog - no ending slash
   },
   mapping: {
     'MarkdownRemark.frontmatter.author': 'AuthorYaml',
